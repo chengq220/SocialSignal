347a5e11711482ec3cf9714c48695356a207983f
chengq220 <chengqiu220@gmail.com>
2025-07-14 20:20:01 -0400

    fix

--- backend/apiRoutes.py
+++ backend/apiRoutes.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter
-from database.database import DBManager
+# from fastapi import APIRouter
+# from database.database import DBManager
 
-router = APIRouter()
-db = DBManager()
+# router = APIRouter()
+# db = DBManager()
 
 
-@router.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Welcome to this amazing project."}
+# @router.get("/", tags=["root"])
+# async def read_root() -> dict:
+#     return {"message": "Welcome to this amazing project."}
 
-@router.get("/subreddits", tags=["subreddits"])
-async def read_subreddit() -> dict:
-    return {"message": "Hi"}
+# @router.get("/subreddits", tags=["subreddits"])
+# async def read_subreddit() -> dict:
+#     return {"message": "Hi"}
