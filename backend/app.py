829d378874e28d55b3f3dd1c66d060b9a47afb87
chengq220 <chengqiu220@gmail.com>
2025-07-14 20:19:48 -0400

    fix

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,30 @@
-from fastapi import FastAPI, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-from apiRoutes import router
-from database.database import DBManager
+# from fastapi import FastAPI, HTTPException, status
+# from fastapi.middleware.cors import CORSMiddleware
+# from contextlib import asynccontextmanager
+# from apiRoutes import router
+# from database.database import DBManager
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000"
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000"
+# ]
 
-db = DBManager()
+# db = DBManager()
 
-@asynccontextmanager
-async def lifespan(app):
-    global db
-    await db.connect()
-    yield
-    await db.disconnect()
+# @asynccontextmanager
+# async def lifespan(app):
+#     global db
+#     await db.connect()
+#     yield
+#     await db.disconnect()
 
-app = FastAPI(lifespan=lifespan)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app = FastAPI(lifespan=lifespan)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
-app.include_router(router)
+# app.include_router(router)
